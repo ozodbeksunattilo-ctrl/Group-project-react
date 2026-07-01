da4573f0ce6ce151a3703e6c9e686dbbc7620ff7
ozodbeksunattilo <ozodbeksunattilo@gmail.com>
2026-07-01 15:29:33 +0500

    React projects

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from "./Components/Loyout/Header/Header"
+import Main from "./Components/Loyout/Footer/Footer"
+import Footer from "./Components/Ui/Main/Main"
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Footer/>
+      <Main/>
+
+
+
+    </div>
+  )
+}
+
+export default App
